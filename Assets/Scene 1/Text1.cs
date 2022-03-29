d3a4ae35aee2b97e032d388205e80817e5534558
IlanRepiso <101400193+IlanRepiso@users.noreply.github.com>
2022-03-29 13:27:28 -0300

    Preparaciones + Ejercicio 1

---
+++ Assets/Scene 1/Text1.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int num1 = 0;
+        num1 += 2;
+        Debug.Log(num1);
+        Debug.Log(num1 * num1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
