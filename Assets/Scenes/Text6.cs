b0e1bb43da80366f8799aa156259c2a7d47a79ed
IlanRepiso <101400193+IlanRepiso@users.noreply.github.com>
2022-03-29 14:20:26 -0300

    Hasta el 6
    noice

---
+++ Assets/Scenes/Text6.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Text6 : MonoBehaviour
+{
+    /*Realizá un programa que permita el ingreso por Inspector de un valor entero mayor que 0
+en una variable llamada num1 y muestre un mensaje por pantalla indicando "el número es
+par" o "el número es impar". Deberá utilizar el operador “módulo” es el caracter %.*/
+
+    public int num1 = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int resultado;
+        resultado = num1 % 2;
+
+        if (resultado == 1)
+        {
+            Debug.Log("el número es impar");
+        }
+        else
+        {
+            Debug.Log("el número es par");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
